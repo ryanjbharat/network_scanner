7012fa457b39650dd8de60d34d1ead1002176c06
Ryan <ryanjbharat@gmail.com>
2020-12-28 23:03:02 -0500

    Setup target IP to be grabbed from command line

--- network_scanner.py
+++ network_scanner.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python3
 
 import scapy.all as scapy
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Scan the IP range.")
+    parser.add_argument("-t", "--target", dest="target", help="Target IP range to scan. /8, /16, /24 only.")
+    args = parser.parse_args()
+    if not args.target:
+        parser.error("Please specify an IP range, use --help for more help.")
+    return args
+
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    packet = broadcast/arp_request
+    packet = broadcast / arp_request
     answered_list = scapy.srp(packet, timeout=1, verbose=False)[0]
-
-    print("IP\t\t\tMAC Address\n---------------------------------------------------")
+    clients_list = []
     for element in answered_list:
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-        print("---------------------------------------------------")
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
 
 
-scan("192.168.226.0/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n---------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+args = get_arguments()
+
+scan_result = scan(args.target)
+print_result(scan_result)
